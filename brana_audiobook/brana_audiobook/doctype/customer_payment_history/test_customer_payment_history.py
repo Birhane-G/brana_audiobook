53a03c153dad51276105df2a9e99e6e55a35ea6d
Birhane Gebriel <Birhanegebrial1@gmail.com>
2023-08-18 23:27:13 +0300

    Added Doctypes

---
+++ brana_audiobook/brana_audiobook/doctype/customer_payment_history/test_customer_payment_history.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Birhane Gebrial and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomerPaymentHistory(FrappeTestCase):
+	pass
---
+++ brana_audiobook/brana_audiobook/doctype/subscriptions_plan/subscriptions_plan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Birhane Gebrial and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Subscriptions Plan', {
+	// refresh: function(frm) {
+
+	// }
+});
