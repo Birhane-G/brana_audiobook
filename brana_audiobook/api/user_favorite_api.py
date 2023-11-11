40a055663ab590519c129d17c65ff2709cb26d6d
Birhane Gebriel <birhanegebrial1@gmail.com>
2023-11-11 19:50:23 +0000

    Favourite

--- brana_audiobook/api/user_favorite_api.py
+++ brana_audiobook/api/user_favorite_api.py
@@ -1,21 +1,36 @@
 import frappe
-
+from frappe import _
 @frappe.whitelist(allow_guest=True)
 def favorite(title):
     if not frappe.session.user:
         frappe.throw("User not authenticated", frappe.AuthenticationError)
-    
     user = frappe.get_doc("User", frappe.session.user)
-    favorite = frappe.new_doc("Brana User Profile")
-    favorite.user = user.email
-    favorite.user_name = user.full_name
-    
-    favorite_item = favorite.append("wish_list", {})
-    favorite_item.title = title
-    favorite_item.favourite = 1
-    
-    favorite.save()
-    
-    return {
-        "message": "sucess"
-    }
+    try:
+        favorite = frappe.get_doc("Brana User Profile", user.email)
+        if favorite:
+            favorite = frappe.get_doc("Brana User Profile", user.email)
+            if not favorite.wish_list:
+                favorite_item = favorite.append("wish_list", {})
+                favorite_item.title = title
+                favorite_item.favourite = 1
+                favorite.save()
+                return "Favourite"
+            else:
+                for item in favorite.wish_list:
+                    if item.title == title:
+                        favorite.remove(item)
+                        favorite.save()
+                        # item.favourite = 0
+                        return "Remove Favourite"
+        else:
+            favorite = frappe.new_doc("Brana User Profile")
+            favorite.user = user.email
+            favorite.user_name = user.full_name
+            favorite_item = favorite.append("wish_list", {})
+            favorite_item.title = title
+            favorite_item.favourite = 1
+            favorite.save()
+            return "Favourite"
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Error")
+        return {"message": _("Favourite Error")}
